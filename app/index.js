4ece804302a607a15667b3346a0d5d6fe54c260a
arunkumarayinabathina <arun987610@gmail.com>
2025-08-10 17:06:15 +0530

    modal height and width modifying

--- app/index.js
+++ app/index.js
@@ -10,17 +10,20 @@ export default function Index() {
       onPress={()=>setIsModalVisible(true)}
       color="#E64F3E"
     />
-    <Modal 
+    <Modal style={{height:400,width:300}}
       visible={isModalVisible}
       onRequestClose={()=>setIsModalVisible(false)}
       animationType="slide" // fade,none,slide
       // presentationStyle="pageSheet"  //  pageSheet,formSheet  for ios works
+      transparent={true}
     >
-      <View style={styles.container1}>
+      <View style={styles.modalBackground}>
+        <View style={styles.modalBox}>
           <Text>Modal Content</Text>
           <Button title="Close" color="midnightblue"
           onPress={()=>setIsModalVisible(false)}
           />
+        </View>
       </View>
     </Modal>
    </View>
@@ -37,5 +40,22 @@ const styles = StyleSheet.create({
   container1:{
     flex:1,
     backgroundColor:'green',
-  }
+  },
+  modalBackground: {
+  flex: 1,
+  backgroundColor: 'rgba(0,0,0,0.5)', // dimmed background
+  justifyContent: 'center',
+  alignItems: 'center',
+},
+modalBox: {
+  width: 300,
+  height: 400,
+  backgroundColor: 'white',
+  borderRadius: 10,
+  padding: 20,
+  shadowColor: '#000',
+  shadowOpacity: 0.25,
+  shadowRadius: 4,
+  elevation: 5,
+},
 })
