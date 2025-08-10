fad6a34754bfdc889cdd280d76584b9e3786a90b
arunkumarayinabathina <arun987610@gmail.com>
2025-08-10 15:43:04 +0530

    Image,ImageBackground

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, View } from "react-native";
+const logoImage = require("../assets/images/adaptive-icon.png")
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>
-        <Text style={{color:'white'}}>
-            Hello
-        </Text>
-         World
-      </Text>
+      {/* <Image source={logoImage} style={{height:200,width:200}}/>
+      <Image source={{uri:"https://picsum.photos/300"}} style={{height:200,width:200}}/> */}
+
+      <ImageBackground source={logoImage} style={{flex:1}}>
+          <Text>Image Text</Text>
+      </ImageBackground>
     </View>
   );
 }
