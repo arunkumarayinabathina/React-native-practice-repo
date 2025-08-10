67e84fdec5b1a4e871a3ebea1266eb936f2ff953
arunkumarayinabathina <arun987610@gmail.com>
2025-08-10 15:51:23 +0530

    scrollView

--- app/index.js
+++ app/index.js
@@ -1,15 +1,18 @@
-import { ImageBackground, StyleSheet, Text, View } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 const logoImage = require("../assets/images/adaptive-icon.png")
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      {/* <Image source={logoImage} style={{height:200,width:200}}/>
-      <Image source={{uri:"https://picsum.photos/300"}} style={{height:200,width:200}}/> */}
-
-      <ImageBackground source={logoImage} style={{flex:1}}>
-          <Text>Image Text</Text>
-      </ImageBackground>
+      <ScrollView>
+      <Image source={logoImage} style={{height:200,width:200}}/>
+      <Text>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac elementum nisi, in pulvinar purus. Vestibulum varius at dui id tristique. Integer pellentesque est ac convallis ornare. Mauris placerat metus nec nunc scelerisque aliquet. In vitae porttitor libero. Ut fringilla faucibus sem, sit amet scelerisque tellus venenatis a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vel ornare justo, sit amet fermentum ipsum. Mauris posuere lacus sed facilisis eleifend. Nulla vitae tellus eu felis venenatis vestibulum at ut velit. Vivamus vel velit placerat, egestas odio a, euismod arcu. Aliquam sollicitudin laoreet ultricies.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac elementum nisi, in pulvinar purus. Vestibulum varius at dui id tristique. Integer pellentesque est ac convallis ornare. Mauris placerat metus nec nunc scelerisque aliquet. In vitae porttitor libero. Ut fringilla faucibus sem, sit amet scelerisque tellus venenatis a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vel ornare justo, sit amet fermentum ipsum. Mauris posuere lacus sed facilisis eleifend. Nulla vitae tellus eu felis venenatis vestibulum at ut velit. Vivamus vel velit placerat, egestas odio a, euismod arcu. Aliquam sollicitudin laoreet ultricies.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac elementum nisi, in pulvinar purus. Vestibulum varius at dui id tristique. Integer pellentesque est ac convallis ornare. Mauris placerat metus nec nunc scelerisque aliquet. In vitae porttitor libero. Ut fringilla faucibus sem, sit amet scelerisque tellus venenatis a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vel ornare justo, sit amet fermentum ipsum. Mauris posuere lacus sed facilisis eleifend. Nulla vitae tellus eu felis venenatis vestibulum at ut velit. Vivamus vel velit placerat, egestas odio a, euismod arcu. Aliquam sollicitudin laoreet ultricies.
+      </Text>
+      <Image source={logoImage} style={{height:200,width:200}}/>
+      </ScrollView>
     </View>
   );
 }
@@ -19,6 +22,7 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor:'plum',
     // alignItems:'center',
-    // justifyContent:'center'
+    // justifyContent:'center',
+    padding:20
   }
 })
