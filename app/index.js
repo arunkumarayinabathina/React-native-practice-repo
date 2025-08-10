d63fc2eb8a3c209b1623b52bbe7471b0d55c69a3
arunkumarayinabathina <arun987610@gmail.com>
2025-08-10 16:56:21 +0530

    modal component

--- app/index.js
+++ app/index.js
@@ -1,29 +1,29 @@
-import { Button, Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Modal, StyleSheet, Text, View } from "react-native";
 const logoImage = require("../assets/images/adaptive-icon.png")
-
 export default function Index() {
+  const [isModalVisible,setIsModalVisible] = useState(false);
   return (
-    // Pressable props 
-    // - onPressIn
-    // - onPressOut
-    // - onPress
-    // - onLongPress
-    // - onPressOut
-    <View style={styles.container}>
-      <Button
-        title="Press"
-        onPress={()=>{console.log("Hello welcome.")}}
-        color="#E64F3E"
-      />
-      <Pressable onPress={()=>console.log("Image Pressed")}>
-       <Image source={logoImage} style={{height:200,width:200}}/>
-      </Pressable>
-      <Pressable onPress={()=>console.log("Text Pressed")}>
-        <Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac elementum nisi, in pulvinar purus. Vestibulum varius at dui id tristique. Integer pellentesque est ac convallis ornare. Mauris placerat metus nec nunc scelerisque aliquet. In vitae porttitor libero. Ut fringilla faucibus sem, sit amet scelerisque tellus venenatis a. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vel ornare justo, sit amet fermentum ipsum. Mauris posuere lacus sed facilisis eleifend. Nulla vitae tellus eu felis venenatis vestibulum at ut velit. Vivamus vel velit placerat, egestas odio a, euismod arcu. Aliquam sollicitudin laoreet ultricies.
-        </Text>
-      </Pressable>
-    </View>
+   <View style={styles.container}>
+    <Button
+      title="Click me"
+      onPress={()=>setIsModalVisible(true)}
+      color="#E64F3E"
+    />
+    <Modal 
+      visible={isModalVisible}
+      onRequestClose={()=>setIsModalVisible(false)}
+      animationType="slide" // fade,none,slide
+      // presentationStyle="pageSheet"  //  pageSheet,formSheet  for ios works
+    >
+      <View style={styles.container1}>
+          <Text>Modal Content</Text>
+          <Button title="Close" color="midnightblue"
+          onPress={()=>setIsModalVisible(false)}
+          />
+      </View>
+    </Modal>
+   </View>
   );
 }
 
@@ -33,6 +33,9 @@ const styles = StyleSheet.create({
     backgroundColor:'#41DEF0',
     // alignItems:'center',
     // justifyContent:'center',
-    padding:20
+  },
+  container1:{
+    flex:1,
+    backgroundColor:'green',
   }
 })
