d4794fa0c3a6c4ecc6c2372f773fc4a4333195bb
arunkumarayinabathina <arun987610@gmail.com>
2025-08-11 14:00:10 +0530

    status bar

--- app/index.js
+++ app/index.js
@@ -1,61 +1,24 @@
 import { useState } from "react";
-import { Button, Modal, StyleSheet, Text, View } from "react-native";
+import { StatusBar, View } from "react-native";
+
 const logoImage = require("../assets/images/adaptive-icon.png")
+
 export default function Index() {
-  const [isModalVisible,setIsModalVisible] = useState(false);
+  const [isStatusVisible, setIsStatusVisible] = useState(false)
   return (
-   <View style={styles.container}>
-    <Button
-      title="Click me"
-      onPress={()=>setIsModalVisible(true)}
-      color="#E64F3E"
-    />
-    <Modal style={{height:400,width:300}}
-      visible={isModalVisible}
-      onRequestClose={()=>setIsModalVisible(false)}
-      animationType="slide" // fade,none,slide
-      // presentationStyle="pageSheet"  //  pageSheet,formSheet  for ios works
-      transparent={true}
-    >
-      <View style={styles.modalBackground}>
-        <View style={styles.modalBox}>
-          <Text>Modal Content</Text>
-          <Button title="Close" color="midnightblue"
-          onPress={()=>setIsModalVisible(false)}
-          />
-        </View>
-      </View>
-    </Modal>
-   </View>
+    <View style={{flex:1,backgroundColor:'orange'}}>
+      {/* <StatusBar
+          backgroundColor='blue'
+          barStyle='light-content'
+          hidden={isStatusVisible}
+      />
+      <Button
+        title={isStatusVisible ? "show status bar" : "close status bar"}
+        onPress={() => setIsStatusVisible(!isStatusVisible)}
+      /> */}
+      <StatusBar
+        backgroundColor='red'
+      />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#41DEF0',
-    // alignItems:'center',
-    // justifyContent:'center',
-  },
-  container1:{
-    flex:1,
-    backgroundColor:'green',
-  },
-  modalBackground: {
-  flex: 1,
-  backgroundColor: 'rgba(0,0,0,0.5)', // dimmed background
-  justifyContent: 'center',
-  alignItems: 'center',
-},
-modalBox: {
-  width: 300,
-  height: 400,
-  backgroundColor: 'white',
-  borderRadius: 10,
-  padding: 20,
-  shadowColor: '#000',
-  shadowOpacity: 0.25,
-  shadowRadius: 4,
-  elevation: 5,
-},
-})
