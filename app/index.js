ad0c3e4348c4ec4132f06bb7e4388d045f0aae0d
arunkumarayinabathina <arun987610@gmail.com>
2025-08-11 21:37:42 +0530

    activity indicator

--- app/index.js
+++ app/index.js
@@ -1,24 +1,26 @@
 import { useState } from "react";
-import { StatusBar, View } from "react-native";
+import { ActivityIndicator, Button, Image, View } from "react-native";
 
 const logoImage = require("../assets/images/adaptive-icon.png")
 
 export default function Index() {
-  const [isStatusVisible, setIsStatusVisible] = useState(false)
+  const [isLoading,setIsLoading] = useState(true); 
+
   return (
-    <View style={{flex:1,backgroundColor:'orange'}}>
-      {/* <StatusBar
-          backgroundColor='blue'
-          barStyle='light-content'
-          hidden={isStatusVisible}
-      />
-      <Button
-        title={isStatusVisible ? "show status bar" : "close status bar"}
-        onPress={() => setIsStatusVisible(!isStatusVisible)}
-      /> */}
-      <StatusBar
-        backgroundColor='red'
-      />
-    </View>
+      <View style={{flex:1,backgroundColor:'lightblue'}}>
+        <ActivityIndicator
+          size={"large"}
+          color={"red"}
+          animating={isLoading}
+        />
+        <Image
+          source={logoImage}
+          style={{width:200,height:200}}
+        />
+        <Button
+        title="Button control"
+        onPress={()=>setIsLoading(!isLoading)}
+        />
+      </View>
   );
 }
